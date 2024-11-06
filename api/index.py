7d3efc8e840c7655f5c622c6802887c1bf2ab5eb
lucaskafeapps <lucas@kafeapps.com.br>
2024-11-06 11:49:53 -0300

    Add chassi consultation functionality

--- api/index.py
+++ api/index.py
@@ -41,6 +41,23 @@ def consultar_placa(placa):
     else:
         return {"error": "Erro na requisição para consultar a placa", "status_code": response.status_code}
 
+def consultar_chassi(chassi):
+    token = gerar_token()
+    if token is None:
+        return {"error": "Token não gerado"}
+
+    url_chassi = f"https://api.datacast3.com/api/v1.0/CodificacaoFipe/chassi/{chassi}"
+    headers_chassi = {
+        "Authorization": f"Bearer {token}",
+        "infocar-id-Key": "225"
+    }
+
+    response = requests.get(url_chassi, headers=headers_chassi)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": "Erro na requisição para consultar o chassi", "status_code": response.status_code}
+
 @app.route('/consulta_placa', methods=['GET'])
 def consulta_placa():
     placa = request.args.get('placa')
@@ -48,6 +65,15 @@ def consulta_placa():
         return jsonify({"error": "Parâmetro 'placa' é obrigatório"}), 400
 
     resultado = consultar_placa(placa)
+    return jsonify(resultado)
+
+@app.route('/consulta_chassi', methods=['GET'])
+def consulta_chassi():
+    chassi = request.args.get('chassi')
+    if not chassi:
+        return jsonify({"error": "Parâmetro 'chassi' é obrigatório"}), 400
+
+    resultado = consultar_chassi(chassi)
     return jsonify(resultado)
 
 # Para que o Vercel entenda o app Flask
